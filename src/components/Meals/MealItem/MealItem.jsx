0e20cc56239602972e3bcc09c2055168de9fdbeb
AgneBalc <agne.martinaityte@gmail.com>
2023-05-04 21:09:39 +0300

    Form and Input components created and styles added

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css'
+import MealItemForm from './MealItemForm';
 
 const MealItem = ({ meal }) => {
   const price = `$${meal.price.toFixed(2)}`;
@@ -11,7 +12,7 @@ const MealItem = ({ meal }) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-
+        <MealItemForm />
       </div>
     </li>
   );
