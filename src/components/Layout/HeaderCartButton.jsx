6c78e2047664825f816532cc7dfbdb0b63181d9c
AgneBalc <agne.martinaityte@gmail.com>
2023-05-05 09:05:13 +0300

    cart context created

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -11,7 +11,9 @@ const HeaderCartButton = ({ onClick }) => {
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>
+      <span
+        className={classes.badge}
+      >
         3
       </span>
     </button>
